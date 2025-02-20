bbbed529c2b6fd5ffef5ce6dc3d5fc84763517b5
Jeni <devjeni315@gmail.com>
2025-02-20 03:42:02 -0500

    #main- Archivos iniciales

---
+++ src/main/java/com/cb/EfCoilaApplication.java
@@ -0,0 +1,13 @@
+package com.cb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfCoilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfCoilaApplication.class, args);
+	}
+
+}
